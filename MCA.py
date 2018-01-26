771bde8daf73ffd2817cb0d0e4801018de9160e9
mohitgoel188 <35655238+mohitgoel188@users.noreply.github.com>
2018-01-26 18:25:53 +0530

    Updated code
    Converted py2 to py3
    Added plotting feature

--- MCA.py
+++ MCA.py
@@ -1,38 +1,41 @@
-sol_name=[]
-criteria_val=[]
-criteria_weight=[]
-criteria_name=[]
-criteria_val.append([])
-criteria_val.append([])
-sum=[]
+import numpy as np
+import matplotlib.pyplot as plt
 
-c=input("\t\tMulti-Criteria Analysis\n\nEnter number of criterias : ")
+c=int(input("\t\tMulti-Criteria Analysis\n\nEnter number of criterias : "))
+criteria_name=np.empty(c,object)
+criteria_weight=np.empty(c,float)
 for i in range(c):
-    criteria_name.append(raw_input("Enter criteria %d name: "%(i+1)))
+    criteria_name[i]=input(f"Enter criteria {i+1} name: ")
 for i in range(c):
-    criteria_weight.append(input("Enter %s weight: "%criteria_name[i]))
-n=input("\nEnter number of possible solutions: ")
+    criteria_weight[i]=float(input(f"Enter {criteria_name[i]} weight: "))
+
+n=int(input("\nEnter number of possible solutions: "))
+criteria_val=np.empty((c,n),float)
+sum=np.empty(n,float)
+sol_name=np.empty(n,object)
 
 for i in range(n):
+    sol_name[i]=input(f"\nEnter solution {i+1} name: ")     
     for j in range(c):
-        criteria_val[i].append(0)
-    sum.append(0)
+        criteria_val[j][i]=float(input(f"Enter value of {criteria_name[j]}: "))
 
 for i in range(n):
-    sol_name.append(raw_input("\nEnter solution %d name: "%(i+1)))     
-    ch=[]
+    div=criteria_val[i].std()
     for j in range(c):
-        criteria_val[j][i]=input("Enter value of %s: "%criteria_name[j])
-
-for i in range(n):
-    div=float(max(criteria_val[i]))
-    for j in range(c):
-        criteria_val[i][j]*=criteria_weight[i]/div
+        criteria_val[j][i]*=criteria_weight[i]/div
 
 for i in range(n):
     for j in range(c):
         sum[i]+=criteria_val[j][i]
 
-i=sum.index(max(sum))    
-print("\nThe most optimal solution is %s"%sol_name[i])
-v=raw_input(" ")
+i=sum.argmax()   
+print(("\nThe most optimal solution is %s"%sol_name[i]))
+
+ind=np.arange(n)
+p=np.empty(n,dtype=object)
+p[0] = plt.bar(ind, criteria_val[0])
+for i in range(1,n):
+    p[i] = plt.bar(ind, criteria_val[i],bottom=criteria_val[i-1])
+plt.xticks(ind,sol_name)
+plt.legend(criteria_name)
+plt.show()
